bdd6ec133a84a32b3962c9211e4b4d4f4cae07a7
Jonai Soto <jonailive@gmail.com>
2019-04-01 22:24:04 -0400

    finishing tutorial

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
-  let temperatureDescription = document.querySelector('.temperature-description')
-  let temperatureDegree = document.querySelector('.temperature-degree')
-  let locationTimeZone = document.querySelector('.location-timezone')
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector('.temperature-degree');
+  let locationTimeZone = document.querySelector('.location-timezone');
+  let temperatureSection = document.querySelector('.temperature');
+  const temperatureSpan = document.querySelector('.temperature span');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -24,7 +26,23 @@ window.addEventListener("load", () => {
                 temperatureDegree.textContent = temperature;
                 temperatureDescription.textContent = summary;
                 locationTimeZone.textContent = data.timezone;
+
+                let celcius = (temperature - 32 ) * (5 / 9);
+
+
                 setIcons(icon, document.querySelector(".icon"));
+
+
+                temperatureSection.addEventListener('click', ()=> {
+                    if (temperatureSpan.textContent === "F"){
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = Math.floor(celcius);
+                    }else {
+                        temperatureSpan.textContent = "F";
+                        temperatureDegree.textContent = temperature
+                    }
+
+                })
 
             });
     });
